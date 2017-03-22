670ce978a442f19843c9bab6cfb589e6f45af5af
Oclemy <oclemmi@gmail.com>
2017-03-22 16:38:19 -0700

    Initial Commit

---
+++ VB ListBox Sort/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿Imports MetroFramework.Forms
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.sortBtn = New MetroFramework.Controls.MetroButton()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(51, 86)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(324, 329)
+        Me.ListBox1.TabIndex = 0
+        '
+        'sortBtn
+        '
+        Me.sortBtn.Location = New System.Drawing.Point(167, 439)
+        Me.sortBtn.Name = "sortBtn"
+        Me.sortBtn.Size = New System.Drawing.Size(107, 33)
+        Me.sortBtn.TabIndex = 1
+        Me.sortBtn.Text = "Sort"
+        Me.sortBtn.UseSelectable = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(541, 495)
+        Me.Controls.Add(Me.sortBtn)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Sort ListBox"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents sortBtn As MetroFramework.Controls.MetroButton
+
+End Class
---
+++ VB ListBox Sort/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    'DECLARATIONS
+    ReadOnly spacecrafts As String() = {"Kepler", "Casini", "Voyager", "New Horizon", "James Web", "Apollo 15", "Enterprise", "WMAP", "Spitzer", "Galileo"}
+    Dim ascending As Boolean = True
+
+    'WHEN FORM IS LOADED
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Populate()
+    End Sub
+
+    'POPULATE
+    Private Sub Populate()
+        ListBox1.Items.Clear()
+        For Each s In spacecrafts
+            ListBox1.Items.Add(s)
+        Next
+    End Sub
+
+    'SORT
+    Private Sub Sort(ByVal asc As Boolean)
+        If asc Then
+            Array.Sort(spacecrafts)
+        Else
+            Array.Reverse(spacecrafts)
+        End If
+
+        'FILL SORTED
+        Populate()
+    End Sub
+
+    'SORT BTN CLICKED
+    Private Sub sortBtn_Click(sender As Object, e As EventArgs) Handles sortBtn.Click
+        Sort(ascending)
+        ascending = Not ascending
+    End Sub
+
+   
+End Class
